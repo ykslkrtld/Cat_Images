4a2f5e7a0e2b9c9b5a43f3efcb972dfb2186ce47
yksl <yukselkurtuldu9@gmail.com>
2024-03-09 11:17:53 +0300

    edit

--- cats.js
+++ cats.js
@@ -1,58 +1,57 @@
-const button = document.querySelector(".btn")
-const clock = document.getElementById("time")
-const loadingDiv = document.querySelector(".loading")
-const containerDiv = document.querySelector(".container")
+const button = document.querySelector(".btn");
+const clock = document.getElementById("time");
+const loadingDiv = document.querySelector(".loading");
+const containerDiv = document.querySelector(".container");
+const catImg = document.querySelector(".pictures");
 
-
-window.onload = () =>{
-    setTimeout(() => {
-        loadingDiv.style.display = "none"
-        containerDiv.classList.remove("d-none")
-        containerDiv.classList.add("d-flex")
-      }, 3000)
-
-      setInterval(showTime, 1000);
-}
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loadingDiv.style.display = "none";
+    containerDiv.style.display = "flex";
+  }, 3000);
+});
 
 const showTime = () => {
   let date = new Date();
-  clock.textContent = date.toLocaleString(); 
-}
-
+  clock.textContent = date.toLocaleString();
+};
 
 const randomCat = async () => {
   try {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-  if(!res.ok){
-    throw new Error(`${res.status}`)
-  }
-  const data = await res.json()
-  displayCats(data)
-
+    // loadingDiv.style.display = "block";
+    // containerDiv.style.display = "none";
+    const res = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=10"
+    );
+    if (!res.ok) {
+      throw new Error(`${res.status}`);
+    }
+    const data = await res.json();
+    displayCats(data);
   } catch (error) {
-    const catImg = document.querySelector(".pictures")
-    catImg.innerHTML = `<img src="./img/error.gif" class="w-100 h-100">`
+    catImg.innerHTML = `<img src="./img/error.gif" class="w-100 h-100">`;
   }
-}
+};
 
 const displayCats = (item) => {
-
-  const catImg = document.querySelector(".pictures")
-  item.forEach(({url}) => {
-      catImg.innerHTML += `
+  // catImg.innerHTML = "";
+  item.forEach(({ url }) => {
+    catImg.innerHTML += `
     <div class="col-12 col-sm-6 col-lg-4 mb-3">
       <div style="height:200px;">
         <img src="${url}" class="w-100 h-100 rounded-4">
       </div>
     </div>
-      `
+      `;
   });
-}
+  // loadingDiv.style.display = "none";
+  // containerDiv.style.display = "flex";
+};
 
-randomCat()
+randomCat();
+setInterval(showTime, 1000);
 
 button.addEventListener("click", () => {
-  const catImg = document.querySelector(".pictures")
-  catImg.innerHTML = ""
-  randomCat()
-})
+  catImg.innerHTML = "";
+  randomCat();
+});
